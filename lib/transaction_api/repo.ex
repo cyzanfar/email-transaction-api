b636efe170583bbc220a6edbe59ca196fd4c9265
cyzanfar <cghazanfar10@gmail.com>
2018-02-11 23:13:00 +0100

    first commit

---
+++ lib/transaction_api/repo.ex
@@ -0,0 +1,11 @@
+defmodule TransactionApi.Repo do
+  use Ecto.Repo, otp_app: :transaction_api
+
+  @doc """
+  Dynamically loads the repository url from the
+  DATABASE_URL environment variable.
+  """
+  def init(_, opts) do
+    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+  end
+end
