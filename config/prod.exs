e90b8913d219d21ff7e6323858a3c1674292b7f2
cyzanfar <cghazanfar10@gmail.com>
2018-02-13 15:51:25 +0100

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -15,12 +15,24 @@ use Mix.Config
 # which you typically run after static files are built.
 config :transaction_api, TransactionApiWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [
+    scheme: "https",
+    host: "fathomless-retreat-94815.herokuapp.com",
+    port: 443
+  ],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
 
+# db configurations
+config :transaction_api, TransactionApi.Repo,
+  adapter: Eco.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE"), || "10"),
+  ssl: true
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
@@ -61,4 +73,6 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
+# commented out because heroku uses env variables
+#
