52e22688e6392c935f5bcec89f4a037f6314bc1d
cyzanfar <cghazanfar10@gmail.com>
2018-02-13 16:00:25 +0100

    typo in db config

--- config/prod.exs
+++ config/prod.exs
@@ -29,7 +29,7 @@ config :logger, level: :info
 
 # db configurations
 config :transaction_api, TransactionApi.Repo,
-  adapter: Eco.Adapters.Postgres,
+  adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
