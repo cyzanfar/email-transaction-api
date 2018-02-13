a07573492f98dd0653476527846dd1ca8ff6fe02
cyzanfar <cghazanfar10@gmail.com>
2018-02-13 15:55:58 +0100

    pool_size config

--- config/prod.exs
+++ config/prod.exs
@@ -31,7 +31,7 @@ config :logger, level: :info
 config :transaction_api, TransactionApi.Repo,
   adapter: Eco.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE"), || "10"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 # ## SSL Support
 #
