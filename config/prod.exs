1c215c9a0eecd45fb4e90b8c5090a5bd08df392d
cyzanfar <cghazanfar10@gmail.com>
2018-02-14 08:44:40 +0100

    remove static manifest

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,6 @@ config :transaction_api, TransactionApiWeb.Endpoint,
     port: 443
   ],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
